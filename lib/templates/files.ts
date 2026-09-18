127cbf8355d1988cbc1f82f6fc1f99ea36d03348
jin <jinsu.kim1011@gmail.com>
2026-09-18 16:44:08 +0200

    refactor(templates): simplify getTemplateDirectory function by removing TemplateKey dependency

--- lib/templates/files.ts
+++ lib/templates/files.ts
@@ -1,14 +1,7 @@
-import { TemplateKey } from "@/types/templates"
 import path from "node:path"
 
 const templateDirectory = path.join(process.cwd(), "app", "templates")
 
-const templateDirectories: Record<TemplateKey, string> = {
-  // when new template added, add directory path
-
-  blank: path.join(templateDirectory, "blank"),
-}
-
-export function getTemplateDirectory(key: TemplateKey): string {
-  return templateDirectories[key]
+export function getTemplateDirectory(slug: string): string {
+  return path.join(templateDirectory, slug)
 }
