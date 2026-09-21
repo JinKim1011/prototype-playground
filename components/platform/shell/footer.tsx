693bbaee2961266496110da62900301eb22c2315
jin <jinsu.kim1011@gmail.com>
2026-09-21 22:10:45 +0200

    feat(footer): adjust padding in Footer component

--- components/platform/shell/footer.tsx
+++ components/platform/shell/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 export function Footer() {
   return (
-    <footer className="flex items-center justify-between border-t-[0.5px] py-2">
+    <footer className="flex items-center justify-between border-t-[0.5px] py-1.5">
       <Link
         href="https://github.com/JinKim1011/prototype-playground"
         className={buttonVariants({
