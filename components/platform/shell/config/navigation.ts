41c5212203f042b25c155711bc0e4c9a801b81fa
jin <jinsu.kim1011@gmail.com>
2026-09-21 15:17:57 +0200

    feat(navigation): add NAV_TABS configuration for navigation links

---
+++ components/platform/shell/config/navigation.ts
@@ -0,0 +1,5 @@
+export const NAV_TABS = [
+  { value: "/prototypes", label: "Prototypes" },
+  { value: "/templates", label: "Templates" },
+  { value: "/design-system", label: "Design System" },
+] as const
