882a41a45587972043c7b7d72995931a1b0b442f
jin <jinsu.kim1011@gmail.com>
2026-09-22 09:47:08 +0200

    refactor(templates): replace TemplateEntry key type to id

--- types/templates.ts
+++ types/templates.ts
@@ -1,9 +1,9 @@
 export type DEFAULT_TEMPLATE_KEY = "blank"
 
 export type TemplateEntry = {
+  id: string
   title: string
   description: string
-  key: string
   slug: string
   owner: string
   createdAt: string
