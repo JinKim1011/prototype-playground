f4be94c2bb439c9427eba20424b50c7e3c3d4db6
jin <jinsu.kim1011@gmail.com>
2026-09-22 15:04:40 +0200

    feat(template): implement TemplatesPage to display a list of templates

--- app/(shell)/templates/page.tsx
+++ app/(shell)/templates/page.tsx
@@ -1,3 +1,18 @@
-export default function TemplatesPage() {
-  return <div>templates</div>
+import { TemplateItemGroup } from "@/components/platform/templates/template-item-group"
+import { getTemplates } from "@/lib/templates/catalog"
+
+export default async function TemplatesPage() {
+  const templates = await getTemplates()
+  return (
+    <TemplateItemGroup
+      templates={templates.map((template) => ({
+        id: template.id,
+        title: template.title,
+        slug: template.slug,
+        createdAt: template.createdAt,
+        updatedAt: template.updatedAt,
+        description: template.description,
+      }))}
+    />
+  )
 }
