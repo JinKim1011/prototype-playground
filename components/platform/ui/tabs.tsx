b1162fd805e3165c553429e79c56e2e94cc8128a
jin <jinsu.kim1011@gmail.com>
2026-09-22 09:15:18 +0200

    feat(tabs): change data-orientation attribute to orientation prop in Tabs component

--- components/platform/ui/tabs.tsx
+++ components/platform/ui/tabs.tsx
@@ -12,7 +12,7 @@ function Tabs({
   return (
     <TabsPrimitive.Root
       data-slot="tabs"
-      data-orientation={orientation}
+      orientation={orientation}
       className={cn(
         "group/tabs flex gap-2 data-[orientation=horizontal]:flex-col",
         className
