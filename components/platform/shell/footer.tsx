74569adbecddfe8bf7e9ed6775387ad8ba005cc9
jin <jinsu.kim1011@gmail.com>
2026-09-23 11:44:02 +0200

    feat(footer): update footer styling and link size for improved UI consistency

--- components/platform/shell/footer.tsx
+++ components/platform/shell/footer.tsx
@@ -1,19 +1,20 @@
 import { buttonVariants } from "@/components/platform/ui/button"
 import { ModeToggle } from "@/components/platform/ui/mode-toggle"
 import Link from "next/link"
+import { ArrowUpRightIcon } from "@phosphor-icons/react/dist/ssr"
 
 export function Footer() {
   return (
-    <footer className="flex items-center justify-between border-t-[0.5px] py-1.5">
+    <footer className="flex items-center justify-between border-t-[0.5px] pt-4">
       <Link
         href="https://github.com/JinKim1011/prototype-playground"
         className={buttonVariants({
           variant: "link",
-          size: "xs",
-          className: "-ml-2",
+          size: "sm",
+          className: "-ml-2 text-muted-foreground!",
         })}
       >
-        GITHUB
+        GitHub
       </Link>
       <ModeToggle />
     </footer>
