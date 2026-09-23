3b152aa5832cffdb9f4aaeae94c6dc83511e5a97
jin <jinsu.kim1011@gmail.com>
2026-09-23 08:55:20 +0200

    feat(template): increase padding in TemplateItemGroup component

--- components/platform/templates/template-item-group.tsx
+++ components/platform/templates/template-item-group.tsx
@@ -10,7 +10,7 @@ type TemplateItemGroupProps = {
 
 export function TemplateItemGroup({ templates }: TemplateItemGroupProps) {
   return (
-    <ItemGroup className="-mx-2 w-[calc(100%+1rem)] py-3">
+    <ItemGroup className="-mx-2 w-[calc(100%+1rem)] py-4">
       {templates.map((template) => (
         <TemplateItem key={template.id} template={template} />
       ))}
