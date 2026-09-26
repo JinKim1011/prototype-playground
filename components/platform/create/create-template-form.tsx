4ddbdcc6c822136440dfa34eeef473114d56caaa
jin <jinsu.kim1011@gmail.com>
2026-09-26 10:43:26 +0200

    feat(create-template-form): restructure form layout and improve accessibility

--- components/platform/create/create-template-form.tsx
+++ components/platform/create/create-template-form.tsx
@@ -5,8 +5,9 @@ import {
   Field,
   FieldDescription,
   FieldGroup,
-  FieldLabel,
+  FieldLegend,
   FieldError,
+  FieldSet,
 } from "@/components/platform/ui/field"
 import { Input } from "@/components/platform/ui/input"
 import { Textarea } from "@/components/platform/ui/textarea"
@@ -35,31 +36,45 @@ export default function CreateTemplateForm() {
 
   return (
     <div className="w-full max-w-md p-4">
-      <form action={formAction}>
+      <form action={formAction} noValidate>
         <FieldGroup>
-          <Field>
-            <FieldLabel htmlFor="">Title</FieldLabel>
-            <Input id="title" placeholder="Enter title" name="title" required />
-            <FieldDescription>
-              Only letters and numbers, no special characters
-            </FieldDescription>
-          </Field>
+          <FieldSet className="gap-2">
+            <Field>
+              <FieldLegend>Details</FieldLegend>
+              <Input
+                id="title"
+                placeholder="Enter title"
+                name="title"
+                required
+                aria-invalid={!!state.errors?.title}
+                aria-describedby={
+                  state.errors?.title ? "title-error" : undefined
+                }
+              />
+              <FieldError id="title-error">{state.errors?.title}</FieldError>
+              {/* <FieldDescription>
+                Only letters and numbers, no special characters
+              </FieldDescription> */}
+            </Field>
+
+            <Field>
+              <Textarea
+                id="description"
+                name="description"
+                placeholder="Description (optional)"
+              />
+            </Field>
+          </FieldSet>
 
           <Field>
-            <Textarea
-              id="description"
-              name="description"
-              placeholder="Description (optional)"
-            />
-          </Field>
-
-          <Field orientation="horizontal">
-            <Button type="submit" disabled={pending}>
-              Create template
+            <Button
+              size="lg"
+              type="submit"
+              disabled={pending}
+              className="mt-1.5 w-full"
+            >
+              Create Template
             </Button>
-            <FieldError>
-              {state.status === "error" ? state.message : undefined}
-            </FieldError>
           </Field>
         </FieldGroup>
       </form>
